45179858ad6ecc1272f475ececf3dcab6c4217bd
Dennis <dennis.spierenburg@hotmail.com>
2018-04-10 10:37:58 +0200

    first init

---
+++ node/modules/router.js
@@ -0,0 +1,46 @@
+var express = require('express')
+var app = express()
+
+var routes = [
+  {
+    location: '/',
+    name: 'home',
+    baseFile: 'index',
+    data: projects[0]
+  }
+];
+
+const router = {
+  init: function(){
+    router.settings();
+    for(let i = 0; i < routes.length; i++){
+      if(routes[i].baseFile){
+        app.get(routes[i].location, function(req, res){
+          var routePath = routes[i].baseFile + '.ejs';
+          res.render(routePath, {data: routes[i].data});
+        });
+      }
+      else{
+        res.status(404);
+        res.render('404.ejs', {url: req.url});
+      }
+    }
+  },
+  settings: function(){
+    //use static files served from public folder
+    app.use(express.static('public'))
+
+    //user view engine ejs
+    app.set('view engine', 'ejs');
+    //get views from src/views folder
+    app.set('views', 'src/views');
+
+  },
+};
+
+var server = app.listen(1337, function () {
+   console.log('server is running on port 1337')
+});
+
+
+module.exports = router;
---
+++ node/server.js
@@ -0,0 +1,5 @@
+var express = require('express');
+app = express();
+var router = require('./modules/router.js');
+
+router.init();
